365547e2eadf65169a76c772f8b9754cb0902a28
Jose Sousa <27413@ufp.edu.pt>
2019-09-23 16:50:17 +0100

    Final commit Fundamentals 3

---
+++ fundamentals-3/fundamentals3/src/com/manchesterdigital/CodeChallenge5.java
@@ -0,0 +1,28 @@
+package com.manchesterdigital;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+public class CodeChallenge5 {
+
+    public static final int CONST_13 = 13;
+
+    public static int luckySum(int a, int b, int c){
+
+        if(a == CONST_13){
+           return 0;
+        }else if(b == CONST_13) {
+            return a;
+        }else if(c == CONST_13){
+            return a + b;
+        }else{
+            return a + b + c;
+        }
+
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(luckySum(1, 2, 3));
+        System.out.println(luckySum(1, 2, 13));
+        System.out.println(luckySum(1, 13, 3));
+    }
+}
--- fundamentals-3/fundamentals3/src/com/manchesterdigital/IfStatements.java
+++ fundamentals-3/fundamentals3/src/com/manchesterdigital/IfStatements.java
@@ -32,19 +32,21 @@ public class IfStatements {
        weekend(5);
     }
 
-    public static void weekend(int var){
-        switch (var){
+    public static void weekend(int var) {
+        switch (var) {
             case 2:
             case 5:
-                System.out.printf("On %s I go to the gym", var == 2? "Tuesday": "Thursday");
+                System.out.printf("On %s I go to the gym", var == 2 ? "Tuesday" : "Thursday");
                 break;
         }
     }
-
+}
+/*
     public static void weekend1(int var){
-        if(var == 1 && var <= 5) {System.out.println("It´s weekday!")};
+        if(var == 1 && var <= 5) {System.out.println("It´s weekday!")}
         System.out.println(2 == var ?"On Tuesday I go to the gym":
                 var == 5? "On Thursday I go to the gym": var == 7?"On Sunday I go to the gym":"");
         }
     }
 }
+*/
